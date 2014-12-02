6c67fdb56d70bc344b5aa0c6572e92fd146fdef8
Bootez <8ootez@gmail.com>
2014-12-02 10:19:46 +0800

    Android Preferences Demo

--- app/src/main/java/com/qomo/preferences/MainActivity.java
+++ app/src/main/java/com/qomo/preferences/MainActivity.java
@@ -1,17 +1,53 @@
 package com.qomo.preferences;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private Button button;
+    private TextView boolTextView;
+    private TextView stringTextView;
+    private TextView multTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        boolTextView = (TextView) findViewById(R.id.tv1);
+        stringTextView = (TextView) findViewById(R.id.tv2);
+        multTextView = (TextView) findViewById(R.id.tv3);
+        button = (Button) findViewById(R.id.btn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences sharedPreferences;
+                sharedPreferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+                //bool
+                boolean b = sharedPreferences.getBoolean(getString(R.string.key_bool), true);
+                boolTextView.setText(b ? "true" : "false");
+
+                //String
+                String s = sharedPreferences.getString(getString(R.string.key_editext), "");
+                stringTextView.setText(s);
+
+                //list
+                String s1 = sharedPreferences.getString(getString(R.string.key_list), "");
+                multTextView.setText(s1);
+
+            }
+        });
     }
 
 
@@ -31,6 +67,8 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
 
